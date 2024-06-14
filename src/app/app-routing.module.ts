63c5d72eff22b3d557dd856a5ad623cf34ab4c31
Joyel <joyel.g@atai.ai>
2024-06-14 15:45:02 +0530

    basic structure implemeted with clean architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './presentation/login/login.component';
+import { RegisterComponent } from './presentation/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component:LoginComponent, path:'login' },
+  { component:RegisterComponent, path:'register' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './presentation/login/login.component';
+import { RegisterComponent } from './presentation/register/register.component';
+import { DataModule } from './data/data.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataModule
   ],
   providers: [],
   bootstrap: [AppComponent]
